033dd9c733c29dc8a28ca92a26d23914504e2c49
Fabian Guevara Pinilla <faba63@hotmail.com>
2021-09-04 09:45:39 -0500

    Agregacion del gitnore

---
+++ creacion-rama-feature.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Script para  crear una rama de feature de desarollo
+"""
+import os
+print("Se va a realizar una rama en el repositorio remoto\n")
+os.system('cmd /k "git status"')
