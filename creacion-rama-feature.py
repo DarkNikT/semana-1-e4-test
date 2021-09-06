0bb8368e7825eeab90f3aa74a6da873617be4398
Fabian Guevara Pinilla <faba63@hotmail.com>
2021-09-06 12:50:44 -0500

    limpieza de rama dev

--- creacion-rama-feature.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Script para  crear una rama de feature de desarollo
-"""
-import os
-print("Se va a realizar una rama en el repositorio remoto\n")
-os.system('cmd /k "git status"')
